3460686e2727c75a8a4ff12edd3bfa3de38a7d2c
NicolasHDAraujo <nicolas_hd.araujo@outlook.com>
2021-12-07 16:56:45 -0300

    introdução ignite

---
+++ 01-fundamentos-reactjs/src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.scss';
+
+import RepositoryList from './components/RepositoryList';
+
+function App() {
+  return (
+    <>
+      <RepositoryList />
+    </>
+  );
+}
+
+export default App;
